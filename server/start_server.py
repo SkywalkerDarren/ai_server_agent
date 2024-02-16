327428fa57c6357441a5c327396ce3fb0f68b36a
SkywalkerDarren <yh83533010yh2@gmail.com>
2024-02-16 23:23:08 +0800

    init

--- server/start_server.py
+++ server/start_server.py
@@ -1,5 +1,6 @@
 import asyncio
 
+from kook_bot.kook_websocket import KookWebsocket
 from ws_service.hearbeat_service import HeartbeatService
 from ws_service.websocket_server import WebSocketServer
 
@@ -9,10 +10,10 @@ async def run():
     ws_server.add_handler(HeartbeatService())
     ws_task = asyncio.create_task(ws_server.start())
 
-    # kook_client = KookBot()
-    # kook_task = asyncio.create_task(kook_client.start())
+    kook_websocket = KookWebsocket()
+    kook_task = asyncio.create_task(kook_websocket.start())
 
-    await asyncio.gather(ws_task)
+    await asyncio.gather(ws_task, kook_task)
 
 
 if __name__ == '__main__':
